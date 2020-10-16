2610eb71ddd09f3a076e7f111ef90b3312c5cad5
tamanowada <72775440+tamanowada@users.noreply.github.com>
2020-10-16 19:26:20 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -7,7 +7,7 @@ function draw() {
   strokeWeight(4);
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
+  fill(255, 182, 193);
   triangle(0,108,106,108,106,0);
   fill(255);
   textSize(32);
