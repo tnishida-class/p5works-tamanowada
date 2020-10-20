0d589ab28856f2d2f0854b14dcf8aff2d5b4b917
tamanowada <72775440+tamanowada@users.noreply.github.com>
2020-10-21 00:17:41 +0900

    Changes 2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,39 @@
 // チェッカー
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   let size = width / 8;
   noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+
+  fill (150);
+  for(let i=0; i<8; i++){
+   for(let j=0; j<8;j++){
+
+     if((i+j)%2==1){
+ rect(30*i,30*j,30,30)}
+
+    }
+    }
+
+
+  for(let i=0; i<8; i++){
+    for(let j=0; j<8; j++){
+
+      if(j<2){
+     fill(255,0,0)
+      }
+
+      if(j==4,j==3){
+        noFill();
+      }
+
+      if(j>4){
+        fill(0)
+      }
+
+
+      if((i+j)%2==1){
+  ellipse(15+i*30,15+j*30,25)
+  }
     }
   }
 }
