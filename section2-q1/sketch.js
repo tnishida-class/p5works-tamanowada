faae5bcaff51e45ad4a7062ad5fccdf774409b61
tamanowada <72775440+tamanowada@users.noreply.github.com>
2020-10-20 13:24:17 +0900

    Changes sectiion2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,19 @@
 // 小手調べ
 function setup(){
   for(let i = 0; i < 10; i++){
+
+    if(i>0,i<5){
+     stroke(255,0,0);
+     }
+       else{
+         stroke(0,0,255);
+       }
+
+       let x=100-i*10;
+      ellipse(50,50,x);
+     }
+   }
+
+
+
     // BLANK[1]
-  }
-}
