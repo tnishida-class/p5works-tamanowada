ae6df9b9f0babf977e2ef22e8b735f352df2d691
tamanowada <72775440+tamanowada@users.noreply.github.com>
2020-10-26 23:46:03 +0900

    Changes 2-q4

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,13 +2,19 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();
   background(255);
 
-  let d = height / 9; // 縞1本の太さ
+  let d = height / 9;
 
   for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    if(i%2==1){
+      fill(255);
+    }
+    else{
+      fill(blue)
+    }
+
     rect(0, i * d, width, (i + 1) * d);
   }
 
@@ -17,5 +23,6 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+  rect(0,40,100,20);
+  rect(40,0,20,100);
 }
