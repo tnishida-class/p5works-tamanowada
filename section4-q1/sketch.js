0db81f5ac92c5d3b1aeb446353beef35a0cd766d
tamanowada <72775440+tamanowada@users.noreply.github.com>
2020-11-13 22:18:43 +0900

    Changes 4-q1

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,11 +1,13 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
   cycle = 100;
+  size = 50;
 }
 
 function draw(){
@@ -13,4 +15,6 @@ function draw(){
   count = (count + 1) % cycle;
   // BLANK[1]
   ellipse(width / 2, height / 2, size);
+  if(count<50){size += 1;}
+  else{size -= 1;}
 }
