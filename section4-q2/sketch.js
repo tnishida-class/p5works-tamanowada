aa6e22922058b6643361f487fef4761d2aee5899
tamanowada <72775440+tamanowada@users.noreply.github.com>
2020-11-23 20:33:20 +0900

    4-q2

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -1,5 +1,5 @@
 // テキスト「アニメーション」
-let x, y, vx, vy;
+let x, y, vx, vy,angle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
@@ -7,11 +7,17 @@ function setup(){
   y = height / 2;
   vx = 8;
   vy = 8;
+  angle = 0;
 }
 
 function draw(){
   background(160, 192, 255);
   // BLANK[2] (hint: 作った star 関数を使います)
+
+  star(x,y,30,angle);
+  angle += 0.1;
+  x += 5;
+  y += 5;
 
   // 端の処理パターン (1) 反対側から出てくる
   if(x > width){ x = 0; }
@@ -23,7 +29,7 @@ function draw(){
 function star(cx, cy, r, angle){
   beginShape();
   for(var i = 0; i < 20; i++){
-    var theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    var theta = TWO_PI * i * 2 / 5 - HALF_PI+ angle;
     // BLANK[1] (hint: angle 分だけ星を回転させるには？)
     var x = cx + cos(theta) * r;
     var y = cy + sin(theta) * r;
